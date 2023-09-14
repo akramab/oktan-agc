a5cdcb720afe0c74491c7c50d98cd9e229bd098c
AlifahRBasyasya <68503436+AlifahRBasyasya@users.noreply.github.com>
2023-09-14 23:33:48 +0700

    Revert "[FE-03] Create UI for Login Page"

--- src/containers/App/reducer.tsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import layoutReducer from "../Layout/reducer";
-
-const Reducer = combineReducers({
-    layoutReducer
-});
-
-export default Reducer;
--- src/containers/App/saga.tsx
+++
@@ -1,8 +0,0 @@
-import { all } from "redux-saga/effects";
-import { watchAppSaga } from "../Layout/saga";
-
-export default function* Saga() {
-    yield all([
-        watchAppSaga()
-    ]);
-}
