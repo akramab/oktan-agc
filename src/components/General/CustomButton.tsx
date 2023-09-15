388ba77dd1067c79d21b4ff407245251f908c6ea
AlifahRBasyasya <alifahbasyasya@gmail.com>
2023-09-15 12:29:00 +0700

    feat: add intro section

--- src/components/General/CustomButton.tsx
+++ src/components/General/CustomButton.tsx
@@ -2,7 +2,7 @@ import { Button } from 'react-bootstrap';
 
 const CustomButton = (props: any) => {
     return (
-        <Button className={`${props?.bg} rounded px-3 border-0 w-100`} onClick={props?.onClick} type="submit">
+        <Button className={`${props?.bg} {props?.width ?? "w-100"} rounded px-3 border-0`} onClick={props?.onClick} type="submit">
             <p className="py-2 text-white">{props?.text}</p>
         </Button>
     )
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -2,6 +2,8 @@ import { lazy } from "react";
 import { Row, Col, Image, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 
 const CustomButton = lazy(() => import("../General/CustomButton"));
 const NavigationBar = lazy(() => import("../../components/General/NavigationBar"));
@@ -11,9 +13,28 @@ const HomeComponent = (props: any) => {
         <div className="h-100 bg-image-full">
             <div className="h-75 d-flex flex-column">
                 <NavigationBar />
-                <Row className="align-items-center flex-grow-1">
+                <Row className="justify-content-center align-items-center flex-grow-1 mt-5">
                     <Col className="text-center">
-                        <h1>HOME</h1>
+                        <Image src={logo} className="width-logo mt-5" />
+                        <h1 className="text-white my-3 fw-bold">OKTAN ITB 2023</h1>
+                        <p className="text-white fs-5">
+                            “OKTAN ITB 2023 sebagai sarana kolaborasi dalam <br/>
+                            inovasi keilmuan kimia untuk pengembangan karya <br/>
+                            yang bermanfaat dan berkelanjutan bagi masyarakat”
+                        </p>
+                        <Row className="justify-content-center mt-4">
+                            <Col xs={2}>
+                                <CustomButton
+                                    bg="bg-purple-bright"
+                                    text={
+                                        <>
+                                            REGISTER NOW
+                                            <FontAwesomeIcon icon={faArrowRightLong} size="xs" className="ms-2" />
+                                        </>
+                                    }
+                                />
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </div>
