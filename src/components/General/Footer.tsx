6e136a2761f6e52f478d64af343f9e2b9a2bcf8c
AlifahRBasyasya <alifahbasyasya@gmail.com>
2023-10-06 15:49:00 +0700

    fix: footer

--- src/components/General/Footer.tsx
+++ src/components/General/Footer.tsx
@@ -1,36 +1,35 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faYoutube, faLinkedin, faTwitter, faFacebookSquare, faTiktok } from '@fortawesome/free-brands-svg-icons';
-import { Link } from "react-router-dom";
 
 const Footer = (props: any) => {
     return (
         <Container fluid className="bg-cyan justify-content-between align-items-center px-5">
             <Row className="justify-content-between align-items-center">
                 <Col xs={12} md={4} xl={3} className="py-3">
-                    <p className="text-white text-center fw-bold fs-7">Copyright © 2022 Oktan ITB</p>
+                    <p className="text-white text-center fw-bold fs-7">Copyright © 2023 Oktan ITB</p>
                 </Col>
                 <Col xs={12} md={5} xl={4} className="brands-logo d-flex align-items-center justify-content-center py-3">
-                    <Link to="https://www.instagram.com/oktanitb/" className="rounded-circle shadow-sm p-3 mx-1 darken-hover">
+                    <a target="_blank" href="https://www.instagram.com/oktanitb/" className="rounded-circle shadow-sm p-3 mx-1 darken-hover">
                         <div className="width-fit d-flex align-items-center">
                             <FontAwesomeIcon icon={faInstagram} size="xl" />
                         </div>
-                    </Link>
-                    <Link to="https://www.youtube.com/channel/UCCVXD3CSaVIHQdNLMXJHLlw" className="rounded-circle shadow-sm p-3 mx-1 darken-hover">
+                    </a>
+                    <a target="_blank" href="https://www.youtube.com/channel/UCCVXD3CSaVIHQdNLMXJHLlw" className="rounded-circle shadow-sm p-3 mx-1 darken-hover">
                         <FontAwesomeIcon icon={faYoutube} size="xl" />
-                    </Link>
-                    <Link to="https://www.linkedin.com/company/oktan-itb" className="rounded-circle shadow-sm p-3 mx-1 darken-hover">
+                    </a>
+                    <a target="_blank" href="https://www.linkedin.com/company/oktan-itb" className="rounded-circle shadow-sm p-3 mx-1 darken-hover">
                         <FontAwesomeIcon icon={faLinkedin} size="xl" />
-                    </Link>
-                    <Link to="https://twitter.com/itboktan" className="rounded-circle shadow-sm p-3 mx-1 darken-hover">
+                    </a>
+                    <a target="_blank" href="https://twitter.com/itboktan" className="rounded-circle shadow-sm p-3 mx-1 darken-hover">
                         <FontAwesomeIcon icon={faTwitter} size="xl" />
-                    </Link>
-                    <Link to="https://www.facebook.com/profile.php?id=100017407802799&_rdc=1&_rdr" className="rounded-circle shadow-sm p-3 mx-1 darken-hover">
+                    </a>
+                    <a target="_blank" href="https://www.facebook.com/profile.php?id=100017407802799&_rdc=1&_rdr" className="rounded-circle shadow-sm p-3 mx-1 darken-hover">
                         <FontAwesomeIcon icon={faFacebookSquare} size="xl" />
-                    </Link>
-                    <Link to="https://www.tiktok.com/@oktanitb" className="rounded-circle shadow-sm p-3 mx-1 darken-hover">
+                    </a>
+                    <a target="_blank" href="https://www.tiktok.com/@oktanitb" className="rounded-circle shadow-sm p-3 mx-1 darken-hover">
                         <FontAwesomeIcon icon={faTiktok} size="xl" />
-                    </Link>
+                    </a>
                 </Col>
             </Row>
         </Container>
